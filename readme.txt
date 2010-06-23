2b92ce89b78893d8fe456ec790eeb668a00d593d
TourCMS <TourCMS@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-06-23 15:02:10 +0000

    Uploading Version 0.4
    git-svn-id: https://plugins.svn.wordpress.org/tour-operator-plugin/tags/0.4@256502 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,54 @@
+=== Tour & Activity Operator Ecommerce Plugin ===
+Contributors: TourCMS
+Tags:
+Version: 0.4
+Plugin URI: http://www.tourcms.com/support/webdesign/wordpress/
+Requires at least: 3.0
+Tested up to: 3.0
+Stable tag: 0.4
+
+
+Adds extra functonality to WordPress to aid creating travel websites.
+
+== Description ==
+
+Build websites selling travel products online.
+
+This plugin extends WordPress with new Post types for Tours/Hotels (and other travel products) and integrates with the TourCMS booking engine. Upload your prices once, ensure you are always displaying live pricing and booking against live availability. 
+
+Includes widgets for displaying products on Google Maps or showing an availability overview and a specially designed theme is available free of charge from our website (or use your existing theme).
+
+Store complex pricing, availability, bookings and customer data secure in [TourCMS](http://www.tourcms.com "TourCMS"). Use WordPress to design and build your website, seamlessly integrating with TourCMS for the booking/enquiry process.
+
+The TourCMS booking engine is configurable in 14+ languages (let us know if you need one we don't currently support) and integrates with various payment gateway solutions from around the world.
+
+== Installation ==
+
+Full installation instructions are available on our website:
+
+[View installation instructions](http://www.tourcms.com/support/webdesign/wordpress/installation.php "Installation instructions")
+
+== Screenshots ==
+
+1. Google Maps Widget
+2. Availability Widget
+3. Some of the Meta data pulled from TourCMS, accessible via Shortcodes
+4. Editing Products types / Locations
+5. Product types / Locations appear in the Menu editor
+
+== Changelog ==
+
+= 0.4 =
+* Added screenshots
+* Improved readme.txt
+
+= 0.3 =
+* Updated lead in prices to use the new 'display' price which was added to the TourCMS API (includes currency symbol / description)
+* Added new Meta to store the monthly availability which was added to the TourCMS API
+* Added new Availability widget
+
+= 0.2 =
+* Documentation improvements
+
+= 0.1 =
+* Initial release
---
+++ widgets/tourAvail/tourAvail.php
@@ -0,0 +1,120 @@
+<?php
+
+class tourAvail extends WP_Widget {
+	function tourAvail() {
+		parent::WP_Widget(false, 'Tour Availability', array('description' => 'Display a two year calendar indicating which months have availability. Only visible on Tour/Hotel pages.'));	
+	}
+	
+	function widget($args, $instance) {
+		global $post;
+		
+		// Check that this is a "tour" and that it has a start location saved
+		// at the time of writing all Tours in TourCMS must have a start location
+		if(is_single() && get_query_var('post_type') == 'tour') {
+			$has_sale = (int)get_post_meta($post->ID, 'tourcms_wp_has_sale', true );
+			if($has_sale==1)
+				$do_output = true;	
+			else
+				$do_output = false;			
+		} else {
+			$do_output = false;
+		}
+		
+		if($do_output) {
+			extract($args);
+			echo $before_widget;
+			$title = empty($instance['title']) ? '&nbsp;' : apply_filters('widget_title', $instance['title']);
+			if ( !empty( $title ) ) { echo $before_title . $title . $after_title; };
+				$year = date("Y");
+				$month = date("n");
+				$months = array("jan","feb","mar","apr","may","jun","jul","aug","sep","oct","nov","dec");
+				$style = get_option('tourcms_wp_bookstyle');
+				
+				// Build Link format (if any)
+				if($style!="off" && $style!="iframe") {
+					$height = (get_option('tourcms_wp_bookheight')=="") ? "600" : get_option('tourcms_wp_bookheight');
+					$width = (get_option('tourcms_wp_bookwidth')=="") ? "600" : get_option('tourcms_wp_bookwidth');			
+					$link = get_post_meta($post->ID, 'tourcms_wp_book_url', true );
+					
+					if($style=="popup") {
+						// Popup window
+						$if_width = (int)$width - 20;
+						$link .= "&if=1&ifwidth=$if_width";
+						$text = '<a href="'.$link.'" onclick="window.open(this, \'_blank\', \'height='.$height.',width='.$width.',statusbar=0,scrollbars=1\'); return false;">[MONTH]</a>';
+					} else {
+						$text = '<a href="'.$link.'">[MONTH]</a>';
+					}
+				} else {
+					$text = "[MONTH]";
+				}
+				
+				// Render bulk of widget
+			?>
+				<p style="margin-bottom: 0;"><?php echo get_post_meta($post->ID, 'tourcms_wp_available', true ); ?></p>
+				<style type="text/css">
+					span.month {
+						text-align: center; font-size: 10px; font-family: arial, helvetic, sans-serif; display: block; width: 28px; color: white; float: left; margin-left: 3px; margin-top: 2px;  -moz-border-radius: 3px; -webkit-border-radius: 3px; border-radius: 3px;
+					}
+					
+					span.month.off {
+						background: #999;
+					}
+					
+					span.month.on {
+						background: #6d9978; 
+					}
+				</style>
+				<div><div style="width: 100px; float: left;">
+					<strong><?php print $year; ?></strong><br />
+					<?php
+						for($i=0; $i<=11; $i++) {
+							if($i<$month-1)
+								print '<span class="month off">'.__( strtoupper($months[$i]), 'tourcms_wp' ).'</span> ';
+							else {
+								$onsale = (int)get_post_meta($post->ID, 'tourcms_wp_has_sale_'.$months[$i], true );
+								if($onsale)
+									print str_replace("[MONTH]", '<span class="month on">'.__( strtoupper($months[$i]), 'tourcms_wp' ).'</span> ', $text);
+								else
+									print '<span class="month off">'.__( strtoupper($months[$i]), 'tourcms_wp' ).'</span> ';		
+							}
+						}
+					?>
+				</div>
+				<div style="width: 100px; float: left;">
+					<strong><?php print $year + 1; ?></strong><br />
+					<?php
+						for($i=0; $i<=11; $i++) {
+							if($i<$month-1) {
+								$onsale = (int)get_post_meta($post->ID, 'tourcms_wp_has_sale_'.$months[$i], true );
+								if($onsale)
+									print str_replace("[MONTH]", '<span class="month on">'.__( strtoupper($months[$i]), 'tourcms_wp' ).'</span> ', $text);
+								else
+									print '<span class="month off">'.__( strtoupper($months[$i]), 'tourcms_wp' ).'</span> ';
+							} else
+								print '<span class="month off">'.__( strtoupper($months[$i]), 'tourcms_wp' ).'</span> ';
+						}
+					?>
+				</div><br style="clear: left;"></div>
+			<?
+			echo $after_widget;
+		}
+	}
+	
+	function update($new_instance, $old_instance) {
+		$instance = $old_instance;
+		$instance['title'] = strip_tags($new_instance['title']);
+		return $instance;
+	}
+	
+	function form($instance) {
+		$instance = wp_parse_args( (array) $instance, array( 'title' => 'Tour Availability') );
+		$title = strip_tags($instance['title']);
+		?>
+		<p><label for="<?php echo $this->get_field_id('title'); ?>">Title: <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo attribute_escape($title); ?>" /></label></p>
+		<?php
+	}
+}
+
+add_action('widgets_init', create_function('', 'return register_widget("tourAvail");'));
+
+?>
